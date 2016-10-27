c6f3204a117a5f81dad303d5e3df04ed2db27514
Chris Underwood <cunderwoodmn@gmail.com>
2016-10-26 17:36:52 -0700

    initial plan outline

---
+++ js/app.js
@@ -0,0 +1,32 @@
+//User interaction doesn't work
+
+//add new ask
+  //add new task
+    //create new list item from new-task
+      //input check box
+      //label
+      //text input
+      //edit button
+      //delete button
+      //each element needs to by modified and appended
+//edit existing task
+  //When edit button is pressed
+    //if the parent has the class .editMode
+      //switch from .editMode
+      //label text to become input's value
+    //else
+      //switch to .editMode
+      //input value becomes label's text
+
+    //toggle .editMode
+
+//delete an existing task
+  //when the delete button is pressed
+    //remove the parent list from the ul
+
+//Mark a task as complete
+  //when the checkbox is checked
+    //append the task list item to the #completed-tasks
+//Mark a task as incomplete
+  //When checkbox is unchecked
+    //append the task list item to the #incomplete-tasks
